e9780048c9ab3744ac44d45224ae7e777e911e2b
Ali Haidar <ahaidar97@outlook.com>
2022-09-29 18:19:16 -0400

    updated db

--- lib/objectbox.g.dart
+++ lib/objectbox.g.dart
@@ -16,43 +16,68 @@ import 'package:objectbox_sync_flutter_libs/objectbox_sync_flutter_libs.dart';
 
 import 'models/exercise.dart';
 import 'models/set.dart';
+import 'models/workout.dart';
 
 export 'package:objectbox/objectbox.dart'; // so that callers only have to import this file
 
 final _entities = <ModelEntity>[
   ModelEntity(
-      id: const IdUid(1, 658129015512293571),
-      name: 'Exercise',
-      lastPropertyId: const IdUid(6, 486059245126040481),
-      flags: 2,
+      id: const IdUid(4, 5556306561390569104),
+      name: 'Set',
+      lastPropertyId: const IdUid(3, 1650906018156953232),
+      flags: 0,
       properties: <ModelProperty>[
         ModelProperty(
-            id: const IdUid(1, 1274146726252656724),
+            id: const IdUid(1, 8739569359095664345),
             name: 'id',
             type: 6,
             flags: 1),
         ModelProperty(
-            id: const IdUid(2, 1748644007173850896),
+            id: const IdUid(2, 264214112287146924),
+            name: 'repetitions',
+            type: 6,
+            flags: 0),
+        ModelProperty(
+            id: const IdUid(3, 1650906018156953232),
+            name: 'date',
+            type: 10,
+            flags: 0)
+      ],
+      relations: <ModelRelation>[],
+      backlinks: <ModelBacklink>[]),
+  ModelEntity(
+      id: const IdUid(5, 5542957906642155572),
+      name: 'Exercise',
+      lastPropertyId: const IdUid(6, 4637854562634076556),
+      flags: 0,
+      properties: <ModelProperty>[
+        ModelProperty(
+            id: const IdUid(1, 416436984501812623),
+            name: 'id',
+            type: 6,
+            flags: 1),
+        ModelProperty(
+            id: const IdUid(2, 8504445299494800586),
             name: 'name',
             type: 9,
             flags: 0),
         ModelProperty(
-            id: const IdUid(3, 9215775960355072575),
+            id: const IdUid(3, 5096844990576401295),
             name: 'target',
             type: 9,
             flags: 0),
         ModelProperty(
-            id: const IdUid(4, 1868734573763643329),
+            id: const IdUid(4, 8764803219866291283),
             name: 'bodyPart',
             type: 9,
             flags: 0),
         ModelProperty(
-            id: const IdUid(5, 4345878167843593260),
+            id: const IdUid(5, 610323052548603806),
             name: 'equipment',
             type: 9,
             flags: 0),
         ModelProperty(
-            id: const IdUid(6, 486059245126040481),
+            id: const IdUid(6, 4637854562634076556),
             name: 'gifUrl',
             type: 9,
             flags: 0)
@@ -60,21 +85,16 @@ final _entities = <ModelEntity>[
       relations: <ModelRelation>[],
       backlinks: <ModelBacklink>[]),
   ModelEntity(
-      id: const IdUid(2, 6674796547357999322),
-      name: 'Set',
-      lastPropertyId: const IdUid(2, 5096502320797118806),
-      flags: 2,
+      id: const IdUid(6, 961841792392638427),
+      name: 'Workout',
+      lastPropertyId: const IdUid(1, 4750836698139640229),
+      flags: 0,
       properties: <ModelProperty>[
         ModelProperty(
-            id: const IdUid(1, 4701210221314884544),
+            id: const IdUid(1, 4750836698139640229),
             name: 'id',
             type: 6,
-            flags: 1),
-        ModelProperty(
-            id: const IdUid(2, 5096502320797118806),
-            name: 'repetitions',
-            type: 6,
-            flags: 0)
+            flags: 1)
       ],
       relations: <ModelRelation>[],
       backlinks: <ModelBacklink>[])
@@ -100,21 +120,64 @@ Future<Store> openStore(
 ModelDefinition getObjectBoxModel() {
   final model = ModelInfo(
       entities: _entities,
-      lastEntityId: const IdUid(2, 6674796547357999322),
+      lastEntityId: const IdUid(6, 961841792392638427),
       lastIndexId: const IdUid(0, 0),
       lastRelationId: const IdUid(0, 0),
       lastSequenceId: const IdUid(0, 0),
-      retiredEntityUids: const [],
+      retiredEntityUids: const [
+        658129015512293571,
+        6674796547357999322,
+        996443871069670041
+      ],
       retiredIndexUids: const [],
-      retiredPropertyUids: const [],
+      retiredPropertyUids: const [
+        1274146726252656724,
+        1748644007173850896,
+        9215775960355072575,
+        1868734573763643329,
+        4345878167843593260,
+        486059245126040481,
+        4701210221314884544,
+        5096502320797118806,
+        916299560753839574
+      ],
       retiredRelationUids: const [],
       modelVersion: 5,
       modelVersionParserMinimum: 5,
       version: 1);
 
   final bindings = <Type, EntityDefinition>{
-    Exercise: EntityDefinition<Exercise>(
+    Set: EntityDefinition<Set>(
         model: _entities[0],
+        toOneRelations: (Set object) => [],
+        toManyRelations: (Set object) => {},
+        getId: (Set object) => object.id,
+        setId: (Set object, int id) {
+          object.id = id;
+        },
+        objectToFB: (Set object, fb.Builder fbb) {
+          fbb.startTable(4);
+          fbb.addInt64(0, object.id);
+          fbb.addInt64(1, object.repetitions);
+          fbb.addInt64(2, object.date.millisecondsSinceEpoch);
+          fbb.finish(fbb.endTable());
+          return object.id;
+        },
+        objectFromFB: (Store store, ByteData fbData) {
+          final buffer = fb.BufferContext(fbData);
+          final rootOffset = buffer.derefObject(0);
+
+          final object = Set(
+              id: const fb.Int64Reader().vTableGet(buffer, rootOffset, 4, 0),
+              repetitions: const fb.Int64Reader()
+                  .vTableGetNullable(buffer, rootOffset, 6),
+              date: DateTime.fromMillisecondsSinceEpoch(
+                  const fb.Int64Reader().vTableGet(buffer, rootOffset, 8, 0)));
+
+          return object;
+        }),
+    Exercise: EntityDefinition<Exercise>(
+        model: _entities[1],
         toOneRelations: (Exercise object) => [],
         toManyRelations: (Exercise object) => {},
         getId: (Exercise object) => object.id,
@@ -164,18 +227,17 @@ ModelDefinition getObjectBoxModel() {
 
           return object;
         }),
-    Set: EntityDefinition<Set>(
-        model: _entities[1],
-        toOneRelations: (Set object) => [],
-        toManyRelations: (Set object) => {},
-        getId: (Set object) => object.id,
-        setId: (Set object, int id) {
+    Workout: EntityDefinition<Workout>(
+        model: _entities[2],
+        toOneRelations: (Workout object) => [],
+        toManyRelations: (Workout object) => {},
+        getId: (Workout object) => object.id,
+        setId: (Workout object, int id) {
           object.id = id;
         },
-        objectToFB: (Set object, fb.Builder fbb) {
-          fbb.startTable(3);
+        objectToFB: (Workout object, fb.Builder fbb) {
+          fbb.startTable(2);
           fbb.addInt64(0, object.id);
-          fbb.addInt64(1, object.repetitions);
           fbb.finish(fbb.endTable());
           return object.id;
         },
@@ -183,10 +245,8 @@ ModelDefinition getObjectBoxModel() {
           final buffer = fb.BufferContext(fbData);
           final rootOffset = buffer.derefObject(0);
 
-          final object = Set(
-              id: const fb.Int64Reader().vTableGet(buffer, rootOffset, 4, 0),
-              repetitions: const fb.Int64Reader()
-                  .vTableGetNullable(buffer, rootOffset, 6));
+          final object = Workout(
+              id: const fb.Int64Reader().vTableGet(buffer, rootOffset, 4, 0));
 
           return object;
         })
@@ -195,37 +255,46 @@ ModelDefinition getObjectBoxModel() {
   return ModelDefinition(model, bindings);
 }
 
-/// [Exercise] entity fields to define ObjectBox queries.
-class Exercise_ {
-  /// see [Exercise.id]
-  static final id = QueryIntegerProperty<Exercise>(_entities[0].properties[0]);
-
-  /// see [Exercise.name]
-  static final name = QueryStringProperty<Exercise>(_entities[0].properties[1]);
-
-  /// see [Exercise.target]
-  static final target =
-      QueryStringProperty<Exercise>(_entities[0].properties[2]);
-
-  /// see [Exercise.bodyPart]
-  static final bodyPart =
-      QueryStringProperty<Exercise>(_entities[0].properties[3]);
-
-  /// see [Exercise.equipment]
-  static final equipment =
-      QueryStringProperty<Exercise>(_entities[0].properties[4]);
-
-  /// see [Exercise.gifUrl]
-  static final gifUrl =
-      QueryStringProperty<Exercise>(_entities[0].properties[5]);
-}
-
 /// [Set] entity fields to define ObjectBox queries.
 class Set_ {
   /// see [Set.id]
-  static final id = QueryIntegerProperty<Set>(_entities[1].properties[0]);
+  static final id = QueryIntegerProperty<Set>(_entities[0].properties[0]);
 
   /// see [Set.repetitions]
   static final repetitions =
-      QueryIntegerProperty<Set>(_entities[1].properties[1]);
+      QueryIntegerProperty<Set>(_entities[0].properties[1]);
+
+  /// see [Set.date]
+  static final date = QueryIntegerProperty<Set>(_entities[0].properties[2]);
+}
+
+/// [Exercise] entity fields to define ObjectBox queries.
+class Exercise_ {
+  /// see [Exercise.id]
+  static final id = QueryIntegerProperty<Exercise>(_entities[1].properties[0]);
+
+  /// see [Exercise.name]
+  static final name = QueryStringProperty<Exercise>(_entities[1].properties[1]);
+
+  /// see [Exercise.target]
+  static final target =
+      QueryStringProperty<Exercise>(_entities[1].properties[2]);
+
+  /// see [Exercise.bodyPart]
+  static final bodyPart =
+      QueryStringProperty<Exercise>(_entities[1].properties[3]);
+
+  /// see [Exercise.equipment]
+  static final equipment =
+      QueryStringProperty<Exercise>(_entities[1].properties[4]);
+
+  /// see [Exercise.gifUrl]
+  static final gifUrl =
+      QueryStringProperty<Exercise>(_entities[1].properties[5]);
+}
+
+/// [Workout] entity fields to define ObjectBox queries.
+class Workout_ {
+  /// see [Workout.id]
+  static final id = QueryIntegerProperty<Workout>(_entities[2].properties[0]);
 }
--- lib/services/objectbox.dart
+++ lib/services/objectbox.dart
@@ -1,34 +1,70 @@
+import 'package:objectbox/objectbox.dart';
+import 'package:pumping_iron/main.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:pumping_iron/models/workout.dart';
+
 import '../models/exercise.dart';
 import '../models/set.dart';
 import '../objectbox.g.dart';
+import '../globals.dart' as globals;
+import 'api_service.dart';
 
 class ObjectBox {
   // the store/database of the app
   late final Store store;
 
-  late final Box<Set> setBox;
-  late final Box<Exercise> exerciseBox;
+  late final Box<Set> setBox; // holds all the sets ever --> filter by date
+  late final Box<Workout> workoutBox;
+  // late final Box<Exercise> exerciseBox;
 
   late final Stream<Query<Set>> setStream;
 
   ObjectBox._create(this.store) {
     setBox = Box<Set>(store);
-    exerciseBox = Box<Exercise>(store);
+    workoutBox = Box<Workout>(store);
+    // exerciseBox = Box<Exercise>(store);
 
-    final qBuilder = setBox.query();
-    setStream = qBuilder.watch(triggerImmediately: true);
+    // final qBuilder = setBox.query();
+    // setStream = qBuilder.watch(triggerImmediately: true);
   }
 
   static Future<ObjectBox> create() async {
+    final docsDir = await getApplicationDocumentsDirectory();
+    final store = await openStore(directory: p.join(docsDir.path, "obx-example"));
     // Future<store> openStore() {...} is defined in the generated objectbox.g.dart
-    final store = await openStore();
+    // final store = await openStore();
     return ObjectBox._create(store);
   }
 
-  List<Exercise> getExercises() {
-    return exerciseBox.getAll();
+  List<Set> getSets() {
+    return setBox.getAll();
   }
 
+  // List<Exercise> getExercises() {return exerciseBox.getAll();}
 
+  // Exercise? getExercise(int id) => exerciseBox.get(id);
 
+  // Stream<List<Exercise>> getExercises() => exerciseBox
+  //     .query()
+  //     .watch(triggerImmediately: true)
+  //     .map((event) => event.find());
+
+  // int insertExercise(Exercise exercise) => exerciseBox.put(exercise);
+
+  // bool deleteExercise(int id) => exerciseBox.remove(id);
+
+  // Future<void> updateDatabase() async {
+  //   print('Before add: ' + globals.exerciseList.length.toString());
+  //   // Get Exercise data
+  //   ApiService apiService = ApiService();
+  //   List<Exercise> futureExercises = await apiService.get(endpoint: 'exercises');
+  //   // Update database and global list
+  //   // exerciseBox.removeAll();
+  //   exerciseBox.putMany(futureExercises);
+  //   globals.exerciseList.removeRange(0, globals.exerciseList.length -1);
+  //   globals.exerciseList.addAll(futureExercises);
+  //   print('DB after add: ' + objectBox.exerciseBox.getAll().length.toString());
+  //   print('Global after add: ' + globals.exerciseList.length.toString());
+  // }
 }
